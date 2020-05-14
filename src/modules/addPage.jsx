8ea341d32c188b403bf191557beba27155216fbe
kyanrong <kekyanrong@gmail.com>
2020-05-14 17:14:12 +0800

    Added build:deploy command + re-added dist/

--- src/modules/addPage.jsx
+++ src/modules/addPage.jsx
@@ -3,19 +3,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import { Card, Title } from './addPage.sc';
+
 class AddPage extends Component {
   render() {
     return (
-      <div>
-        Add
-      </div>
+      <React.Fragment>
+        <Card>
+          <Title>1. Add Place</Title>
+          <span>
+          name, address.street, address.unit, address.building, address.postal, isHalal
+          </span>
+        </Card>
+        <Card>
+          <Title>2. Add Visit</Title>
+          <span>
+            PlaceId, rating, isSponsored, dateOfUpload, youtubeUrl
+          </span>
+        </Card>
+        <Card>
+          <Title>3. Add Foods</Title>
+          <span>
+            PlaceId, VisitId, priceMin, priceMax, hasPriceRange, notes, currency
+          </span>
+        </Card>
+      </React.Fragment>
     );
   }
 }
 
-// AddPage.propTypes = {
+AddPage.propTypes = {
 
-// };
+};
 
 export default connect(
   state => ({
--- src/modules/addPage.sc.jsx
+++ src/modules/addPage.sc.jsx
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
+export const Card = styled.div`
+  background-color: #f4f4f4;
+  padding: 25px 30px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 15px;
+  width: 100%;
+`;
 
+export const Title = styled.h2`
+  margin: 0;
+  font-weight: bold;
+`;
